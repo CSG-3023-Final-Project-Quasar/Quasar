0319f5ca9735adabbee9d9cc006324c9461c0988
ThePerfectPixl <kylelmars@gmail.com>
2023-04-25 12:42:34 -0500

    Revert "Merge branch 'Merged' into Pilar"
    This reverts commit a9757c3560fca4c88b9905398008f765baef33e0, reversing
    changes made to 736fd495eada3ac579078191b84e16378365f638.

--- Quasar/Assets/_Scripts/Player.cs
+++ Quasar/Assets/_Scripts/Player.cs
@@ -1,9 +1,7 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using UnityEngine;
 
@@ -14,28 +12,17 @@ public class Player : MonoBehaviour
     private bool jump;
     private bool collide;
 
-    private bool redOn;
-
     public float grav;
-    public Material blueMat;
-    public Material redMat;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         grav = -9.8f;
-        redOn = false;
-
-        Physics.IgnoreLayerCollision(0, 11, true);
-        Physics.IgnoreLayerCollision(0, 12, false);
-        redMat.SetColor("_Color", new Color(255, 0, 0, .1f));
-        blueMat.SetColor("_Color", new Color(0, 0, 255, 1f));
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.WakeUp();
         if (!collide)
         {
             Vector3 pos = transform.position;
@@ -48,38 +35,18 @@ public class Player : MonoBehaviour
             if(Physics.gravity.y < 0) rb.velocity = new Vector3(rb.velocity.x, 7, 0);
             if(Physics.gravity.y > 0) rb.velocity = new Vector3(rb.velocity.x, -7, 0);
         }
-
-        if(Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            if (redOn)
-            {
-                Physics.IgnoreLayerCollision(0, 11, true);
-                Physics.IgnoreLayerCollision(0, 12, false);
-                redOn = false;
-                redMat.SetColor("_Color", new Color(255, 0, 0, .1f));
-                blueMat.SetColor("_Color", new Color(0, 0, 255, 1f));
-            }
-            else
-            {
-                Physics.IgnoreLayerCollision(0, 11, false);
-                Physics.IgnoreLayerCollision(0, 12, true);
-                redOn = true;
-                redMat.SetColor("_Color", new Color(255, 0, 0, 1f));
-                blueMat.SetColor("_Color", new Color(0, 0, 255, .1f));
-            }
-        }
     }
 
     void OnCollisionEnter(Collision col)
     {
-        //Don't stop on colliding with the ground, only obstacles.
-        if (col.gameObject.tag == "Platform")
+        //Don't collide with the ground, only obstacles.
+        if (col.gameObject.layer != 7)
         collide = true;
     }
 
     void OnCollisionExit(Collision col)
     {
-        if(col.gameObject.tag == "Platform")
+        if(col.gameObject.layer != 7)
         collide = false;
     }
 }
